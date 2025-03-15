31614260e7444e3e9ac10a7a41bd5d5894cd41cb
metwse <108795071+metwse@users.noreply.github.com>
2025-03-15 23:57:51 +0300

    minor: glow effect at the bottom / glow z-index fix

--- src/pages/faq/Faq.jsx
+++ src/pages/faq/Faq.jsx
@@ -1,5 +1,6 @@
 import Footer from '../../components/footer/Footer';
 import Question from './Question';
+import Glow from '../../components/glow/Glow';
 
 import styles from './Faq.module.scss'
 
@@ -22,8 +23,18 @@ export default function Faq() {
                         question="NULL"
                         description="NULL"
                     />
+                    <Question 
+                        question="NULL"
+                        description="NULL"
+                    />
+                    <Question 
+                        question="NULL"
+                        description="NULL"
+                    />
                 </div>
             </div>
+            <Glow className={styles['glow1']} />
+            <Glow className={styles['glow2']} />
             <Footer />
         </section>
     );
--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from 'react'
 
-import Glow from '../../components/glare/Glow.jsx'
+import Glow from '../../components/glow/Glow.jsx'
 import styles from './Homepage.module.scss'
 
 
