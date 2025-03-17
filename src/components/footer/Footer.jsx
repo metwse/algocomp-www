0d16543d8afebb253274015f9ea4abd714ebfe7e
metwse <108795071+metwse@users.noreply.github.com>
2025-03-17 13:15:16 +0300

    minor: sponsors padding / navigation buttons

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,35 +1,36 @@
-import styles from "./Footer.module.scss";
+import styles from "./Footer.module.scss"
+
 
 export default function Footer() {
-  return (
-    <footer className={styles["footer"]}>
-      <img src="/svg/logo-full.svg" />
-      <div>
-        <a href="mailto:ituacm@gmail.com" target="_blank">
-          <svg href="" viewBox="0 0 300 300" xmlns="http://www.w3.org/2000/svg">
-            <path
-              fill="currentColor"
-              d="M275,66.56V255.1c0,.85-.96,1.28-1.6,.85l-50.09-34.77c-.32-.11-.43-.43-.43-.85v-85.28c0-.43-.53-.64-.85-.32l-71.46,60.93c-.32,.32-.96,.32-1.28,0l-71.46-60.93c-.32-.32-.85-.11-.85,.32v85.18c0,.32-.11,.53-.43,.85l-49.98,34.77c-.74,.53-1.6,0-1.6-.85V66.56c0-.32,.11-.53,.32-.85l24.99-21.59c.32-.32,.96-.32,1.28,0l97.62,83.05c.32,.32,.96,.32,1.28,0L248.1,44.23c.32-.32,.96-.32,1.28,0l24.99,21.59c.53,.11,.64,.43,.64,.74h0Z"
-            ></path>
-            <polygon
-              fill="none"
-              points="300 100 200 100 200 0 100 0 100 100 0 100 0 200 100 200 100 300 200 300 200 200 300 200"
-            ></polygon>
-          </svg>
-        </a>
-        <a href="https://github.com/ituacm" target="_blank">
-          <i className="bi bi-github"></i>
-        </a>
-        <a href="https://www.instagram.com/ituacmsc/" target="_blank">
-          <i className="bi bi-instagram"></i>
-        </a>
-        <a href="https://twitter.com/ituacmsc" target="_blank">
-          <i className="bi bi-twitter-x"></i>
-        </a>
-        <a href="https://www.linkedin.com/company/ituacmsc/" target="_blank">
-          <i className="bi bi-linkedin"></i>
-        </a>
-      </div>
-    </footer>
-  );
+    return (
+        <footer className={styles["footer"]}>
+            <img src="/svg/logo-full.svg" />
+            <div>
+                <a href="https://metw.cc" target="_blank">
+                    <svg href="" viewBox="0 0 300 300" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            fill="currentColor"
+                            d="M275,66.56V255.1c0,.85-.96,1.28-1.6,.85l-50.09-34.77c-.32-.11-.43-.43-.43-.85v-85.28c0-.43-.53-.64-.85-.32l-71.46,60.93c-.32,.32-.96,.32-1.28,0l-71.46-60.93c-.32-.32-.85-.11-.85,.32v85.18c0,.32-.11,.53-.43,.85l-49.98,34.77c-.74,.53-1.6,0-1.6-.85V66.56c0-.32,.11-.53,.32-.85l24.99-21.59c.32-.32,.96-.32,1.28,0l97.62,83.05c.32,.32,.96,.32,1.28,0L248.1,44.23c.32-.32,.96-.32,1.28,0l24.99,21.59c.53,.11,.64,.43,.64,.74h0Z"
+                        ></path>
+                        <polygon
+                            fill="none"
+                            points="300 100 200 100 200 0 100 0 100 100 0 100 0 200 100 200 100 300 200 300 200 200 300 200"
+                        ></polygon>
+                    </svg>
+                </a>
+                <a href="https://github.com/ituacm" target="_blank">
+                    <i className="bi bi-github"></i>
+                </a>
+                <a href="https://www.instagram.com/ituacmsc/" target="_blank">
+                    <i className="bi bi-instagram"></i>
+                </a>
+                <a href="https://twitter.com/ituacmsc" target="_blank">
+                    <i className="bi bi-twitter-x"></i>
+                </a>
+                <a href="https://www.linkedin.com/company/ituacmsc/" target="_blank">
+                    <i className="bi bi-linkedin"></i>
+                </a>
+            </div>
+        </footer>
+    );
 }
--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,39 +1,43 @@
-import { useRef, useEffect } from "react";
-import styles from "./Navigation.module.scss";
+import { useRef, useEffect } from 'react'
+
+import styles from './Navigation.module.scss'
+
 
 export default function Navigation() {
-  const headerRef = useRef(null);
+    const headerRef = useRef(null);
 
-  useEffect(() => {
-    const handleResize = () => {
-      document.documentElement.style.setProperty(
-        "--scroll-padding",
-        headerRef.current.offsetHeight +
-          parseFloat(
-            window
-              .getComputedStyle(headerRef.current)
-              .getPropertyValue("margin-top")
-          ) +
-          "px"
-      );
-    };
-    handleResize();
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, [headerRef]);
+    useEffect(() => {
+        const handleResize = () => {
+            document.documentElement.style.setProperty(
+                '--scroll-padding',
+                headerRef.current.offsetHeight +
+                    parseFloat(
+                        window
+                            .getComputedStyle(headerRef.current)
+                            .getPropertyValue('margin-top')
+                    ) + 'px'
+            );
+        };
+        handleResize();
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, [headerRef]);
 
-  return (
-    <header ref={headerRef} className="bd">
-      <a href="/#homepage">
-        <img src="/svg/logo-full.svg" />
-      </a>
-      <div className={styles["buttons"]}>
-        <a href="/#details">Hakkında</a>
-        <a href="/#timeline">Zaman Çizelgesi</a>
-        <a href="/#format">Detaylar</a>
-        <a href="/#faq">SSS</a>
-      </div>
-      <button className="rounded-acm-button">Kayıt</button>
-    </header>
-  );
+    return (
+        <header ref={headerRef} className="bd">
+            <a href="/#homepage">
+                <img src="/svg/logo-full.svg" />
+            </a>
+            <div className={styles['buttons']}>
+                <a href="/#details" className={styles['wide-screen-button']}>
+                    Hakkında
+                </a>
+                <a href="/#timeline">Zaman Çizelgesi</a>
+                <a href="/#awards">Ödüller</a>
+                <a href="/#format">Format</a>
+                <a href="/#faq">SSS</a>
+            </div>
+            <button className="rounded-acm-button">Kayıt</button>
+        </header>
+    );
 }
